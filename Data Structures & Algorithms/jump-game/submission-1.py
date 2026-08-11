23fa23ac0d713fdc5aca27587b14bf5a65059ff9
Adam Lisik <adamlisik01@gmail.com>
2026-08-11 14:11:25 +0200

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        can_jump_to = 0
+        for i in range(len(nums)):
+            if can_jump_to < i: break
+            can_jump_to = max(i + nums[i], can_jump_to)
+
+        return can_jump_to >= len(nums) - 1
+        
